29702169d944720891f012d4ce116206d52df843
lixiaolei19821218 <735534185@qq.com>
2015-08-06 22:31:26 +0800

    bpost post

--- cart/Cart.aspx.cs
+++ cart/Cart.aspx.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading;
@@ -10,6 +12,8 @@ using System.Web;
 using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
 using WMOrderService;
 
 public partial class cart_Cart : System.Web.UI.Page
@@ -422,6 +426,217 @@ public partial class cart_Cart : System.Web.UI.Page
         catch (SmtpException ex)
         {
             return false;
+        }
+    }
+}
+
+public class PostHelp
+{
+    public string GetWebContent(string url)
+    {
+        Stream outstream = null;
+        Stream instream = null;
+        StreamReader sr = null;
+        HttpWebResponse response = null;
+        HttpWebRequest request = null;
+        // 要注意的这是这个编码方式，还有内容的Xml内容的编码方式
+        Encoding encoding = Encoding.GetEncoding("UTF-8");
+        byte[] data = encoding.GetBytes(url);
+
+        // 准备请求,设置参数
+        request = WebRequest.Create(url) as HttpWebRequest;
+        request.Method = "POST";
+        request.ContentType = "text/xml";
+        //request.ContentLength = data.Length;
+
+        outstream = request.GetRequestStream();
+        outstream.Write(data, 0, data.Length);
+        outstream.Flush();
+        outstream.Close();
+        //发送请求并获取相应回应数据
+
+        response = request.GetResponse() as HttpWebResponse;
+        //直到request.GetResponse()程序才开始向目标网页发送Post请求
+        instream = response.GetResponseStream();
+
+        sr = new StreamReader(instream, encoding);
+        //返回结果网页(html)代码
+
+        string content = sr.ReadToEnd();
+        return content;
+    }
+    public string PostXml(string url, string strPost)
+    {
+        string result = "";
+
+        StreamWriter myWriter = null;
+        HttpWebRequest objRequest = (HttpWebRequest)WebRequest.Create(url);
+        objRequest.Method = "POST";
+        //objRequest.ContentLength = strPost.Length;
+        objRequest.ContentType = "text/xml";//提交xml 
+        //objRequest.ContentType = "application/x-www-form-urlencoded";//提交表单
+        try
+        {
+            myWriter = new StreamWriter(objRequest.GetRequestStream());
+            myWriter.Write(strPost);
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+        finally
+        {
+            myWriter.Close();
+        }
+
+        HttpWebResponse objResponse = (HttpWebResponse)objRequest.GetResponse();
+        using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
+        {
+            result = sr.ReadToEnd();
+            sr.Close();
+        }
+        return result;
+    }
+}
+
+public class XMLHelp
+{
+    private XDocument _document;
+
+    public XDocument Document
+    {
+        get { return _document; }
+        set { _document = value; }
+    }
+    private string _fPath = "";
+
+    public string FPath
+    {
+        get { return _fPath; }
+        set { _fPath = value; }
+    }
+
+    /// <summary>
+    /// 初始化数据文件，当数据文件不存在时则创建。
+    /// </summary>
+    public void Initialize()
+    {
+        if (!File.Exists(this._fPath))
+        {
+            this._document = new XDocument(
+            new XElement("entity", string.Empty)
+            );
+            this._document.Save(this._fPath);
+        }
+        else
+            this._document = XDocument.Load(this._fPath);
+    }
+
+    public void Initialize(string xmlData)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(xmlData);
+
+        this._document = XmlDocumentExtensions.ToXDocument(doc, LoadOptions.None);
+    }
+    /// <summary>
+    /// 清空用户信息
+    /// </summary>
+    public void ClearGuest()
+    {
+        XElement root = this._document.Root;
+        if (root.HasElements)
+        {
+            XElement entity = root.Element("entity");
+            entity.RemoveAll();
+        }
+        else
+            root.Add(new XElement("entity", string.Empty));
+    }
+
+
+    ///LYJ 修改
+    /// <summary>
+    /// 提交并最终保存数据到文件。
+    /// </summary>
+
+    public void Commit()
+    {
+        try
+        {
+            this._document.Save(this._fPath);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message, ex);
+        }
+    }
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    public void UpdateQrState(string PId, string state)
+    {
+        XElement root = this._document.Root;
+        XElement entity = root.Element("entity");
+
+        IEnumerable<XElement> elements = entity.Elements().Where(p =>
+        p.Attribute("PId").Value == PId);
+        if (elements.Count() == 0)
+            return;
+        else
+        {
+            XElement guest = elements.First();
+            guest.Attribute("FQdState").Value = state;
+            guest.Attribute("FQdTime").Value = DateTime.Now.ToString();
+            Commit();
+        }
+    }
+
+    public IEnumerable<XElement> GetXElement()
+    {
+        XElement root = this._document.Root;
+        IEnumerable<XElement> elements = root.Elements();
+        return elements;
+    }
+
+
+
+    public DataTable GetEntityTable()
+    {
+        DataTable dtData = new DataTable();
+        XElement root = this._document.Root;
+        IEnumerable<XElement> elements = root.Elements();
+
+        foreach (XElement item in elements)
+        {
+            dtData.Columns.Add(item.Name.LocalName);
+        }
+        DataRow dr = dtData.NewRow();
+        int i = 0;
+        foreach (XElement item in elements)
+        {
+            dr[i] = item.Value;
+            i = i + 1;
+        }
+        dtData.Rows.Add(dr);
+        return dtData;
+    }
+
+}
+
+public static class XmlDocumentExtensions
+{
+    public static XDocument ToXDocument(this XmlDocument document)
+    {
+        return document.ToXDocument(LoadOptions.None);
+    }
+
+    public static XDocument ToXDocument(this XmlDocument document, LoadOptions options)
+    {
+        using (XmlNodeReader reader = new XmlNodeReader(document))
+        {
+            return XDocument.Load(reader, options);
         }
     }
 }
